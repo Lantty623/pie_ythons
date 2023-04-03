16c7d4790dc2aaf54f818df1d44fd66e61dd660d
spobannon <101606056+spobannon@users.noreply.github.com>
2023-04-03 18:01:46 -0400

    Added files for viewModel Implementation
    Added DataState.kt, MainActivity.kt, Screen.kt, and ViewModelTest.kt to use viewModel()

--- MainActivity.kt
+++ MainActivity.kt
@@ -26,9 +26,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if(!Python.isStarted()) {
-            Python.start(AndroidPlatform(this))
-        }
+//        if(!Python.isStarted()) {
+//            Python.start(AndroidPlatform(this))
+//        }
 
         setContent {
             MyApplication2Theme {
@@ -39,122 +39,123 @@ class MainActivity : ComponentActivity() {
                 ) {
                     //Greeting("Android")
                     //test_python()
-                    set_layout()
+                    //set_layout()
+                    Screen()
                 }
             }
         }
     }
 }
-
-@Composable
-fun show_steps(steps: Int) {
-    Surface(color = Color.LightGray) {
-        Text(text = "You've walked $steps steps!")
-    }
-}
-
-@Composable
-fun show_animal_steps(animal: String, steps: Int) {
-    var animal_steps = steps * 100
-    Surface(color = Color.Gray) {
-        Text(text = "That's the equivalent to $animal_steps $animal steps!")
-    }
-}
-
-@Composable
-fun test_python() {
-    val py = Python.getInstance()
-    val module = py.getModule("test")
-    val t = module.callAttr("test").toJava(String::class.java)
-    
-    Text(text = t)
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Composable
-fun set_layout() {
-    Column {
-        val animal =  dropdown_menu()
-        show_steps(2000)
-        show_animal_steps(animal, 2000)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyApplication2Theme {
-        //Greeting("Android")
-        //test_python()
-        set_layout()
-    }
-}
-
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun dropdown_menu(): String {
-    val contextForToast = LocalContext.current.applicationContext
-    val listItems = arrayOf("Dog", "Cat", "Moose", "Duck", "Elephant")
-
-    // state of the menu
-    var expanded by remember {
-        mutableStateOf(false)
-    }
-
-    // remember the selected item
-    var selectedItem by remember {
-        mutableStateOf(listItems[0])
-    }
-
-    // box
-    ExposedDropdownMenuBox(
-        expanded = expanded,
-        onExpandedChange = {
-            expanded = !expanded
-        }
-    ) {
-        TextField(
-            value = selectedItem,
-            onValueChange = { selectedItem = it },
-            label = { Text(text = "Choose an animal") },
-            trailingIcon = {
-                ExposedDropdownMenuDefaults.TrailingIcon(
-                    expanded = expanded
-                )
-            },
-            colors = ExposedDropdownMenuDefaults.textFieldColors()
-        )
-
-        // filter options based on text field value
-        val filteringOptions =
-            listItems.filter { it.contains(selectedItem, ignoreCase = true) }
-
-        if (filteringOptions.isNotEmpty()) {
-            // menu
-            ExposedDropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false }
-            ) {
-                // this is a column scope
-                // all the items are added vertically
-                filteringOptions.forEach { selectionOption ->
-                    // menu item
-                    DropdownMenuItem(
-                        onClick = {
-                            selectedItem = selectionOption
-                            Toast.makeText(contextForToast, selectedItem, Toast.LENGTH_SHORT).show()
-                            expanded = false
-                        }
-                    ) {
-                        Text(text = selectionOption)
-                    }
-                }
-            }
-        }
-    }
-    return selectedItem.lowercase()
-}
+//
+//@Composable
+//fun show_steps(steps: Int) {
+//    Surface(color = Color.LightGray) {
+//        Text(text = "You've walked $steps steps!")
+//    }
+//}
+//
+//@Composable
+//fun show_animal_steps(animal: String, steps: Int) {
+//    var animal_steps = steps * 100
+//    Surface(color = Color.Gray) {
+//        Text(text = "That's the equivalent to $animal_steps $animal steps!")
+//    }
+//}
+//
+//@Composable
+//fun test_python() {
+//    val py = Python.getInstance()
+//    val module = py.getModule("test")
+//    val t = module.callAttr("test").toJava(String::class.java)
+//
+//    Text(text = t)
+//}
+//
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Composable
+//fun set_layout() {
+//    Column {
+//        val animal =  dropdown_menu()
+//        show_steps(2000)
+//        show_animal_steps(animal, 2000)
+//    }
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    MyApplication2Theme {
+//        //Greeting("Android")
+//        //test_python()
+//        set_layout()
+//    }
+//}
+//
+//@OptIn(ExperimentalMaterialApi::class)
+//@Composable
+//fun dropdown_menu(): String {
+//    val contextForToast = LocalContext.current.applicationContext
+//    val listItems = arrayOf("Dog", "Cat", "Moose", "Duck", "Elephant")
+//
+//    // state of the menu
+//    var expanded by remember {
+//        mutableStateOf(false)
+//    }
+//
+//    // remember the selected item
+//    var selectedItem by remember {
+//        mutableStateOf(listItems[0])
+//    }
+//
+//    // box
+//    ExposedDropdownMenuBox(
+//        expanded = expanded,
+//        onExpandedChange = {
+//            expanded = !expanded
+//        }
+//    ) {
+//        TextField(
+//            value = selectedItem,
+//            onValueChange = { selectedItem = it },
+//            label = { Text(text = "Choose an animal") },
+//            trailingIcon = {
+//                ExposedDropdownMenuDefaults.TrailingIcon(
+//                    expanded = expanded
+//                )
+//            },
+//            colors = ExposedDropdownMenuDefaults.textFieldColors()
+//        )
+//
+//        // filter options based on text field value
+//        val filteringOptions =
+//            listItems.filter { it.contains(selectedItem, ignoreCase = true) }
+//
+//        if (filteringOptions.isNotEmpty()) {
+//            // menu
+//            ExposedDropdownMenu(
+//                expanded = expanded,
+//                onDismissRequest = { expanded = false }
+//            ) {
+//                // this is a column scope
+//                // all the items are added vertically
+//                filteringOptions.forEach { selectionOption ->
+//                    // menu item
+//                    DropdownMenuItem(
+//                        onClick = {
+//                            selectedItem = selectionOption
+//                            Toast.makeText(contextForToast, selectedItem, Toast.LENGTH_SHORT).show()
+//                            expanded = false
+//                        }
+//                    ) {
+//                        Text(text = selectionOption)
+//                    }
+//                }
+//            }
+//        }
+//    }
+//    return selectedItem.lowercase()
+//}
